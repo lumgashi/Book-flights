dd146d93fa0551874c80e7e7581b0a29e80e0ec3
lumgashi <lumgashi97@hotmail.com>
2022-05-30 21:01:27 +0200

    front-end-commits

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,128 @@
+import { faBed, faCalendarDays, faCar, faPerson, faPhotoFilm, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import './header.css'
+import {useState} from 'react'
+import { DateRange } from 'react-date-range'
+import {format} from 'date-fns'
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
+const Header = ({type}) => {
+
+  const [openDate, setOpenDate] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection'
+    }
+  ]);
+
+
+  const [openOpsions, setOpenOpsions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
+
+
+  const handleOption = (name, operation) => {
+    
+    setOptions(prev=>{return {...prev, [name] : operation === 'i' ? options[name] + 1 : options[name] - 1}});
+
+  }
+
+  return (
+    <div className='header'>
+          <div  className={type === 'list' ? 'headerContainer listMode' : "headerContainer"}>
+            <div className="headerList">
+                  <div className="headerListItem active">
+                    <FontAwesomeIcon icon={faBed} />
+                    <span>Stays</span>
+                  </div>
+                  <div className="headerListItem">
+                    <FontAwesomeIcon icon={faPlane} />
+                    <span>Flights</span>
+                  </div>
+                  <div className="headerListItem">
+                    <FontAwesomeIcon icon={faCar} />
+                    <span>Car Rentals</span>
+                  </div>
+                  <div className="headerListItem">
+                    <FontAwesomeIcon icon={faPhotoFilm} />
+                    <span>Attractions</span>
+                  </div>
+                  <div className="headerListItem">
+                    <FontAwesomeIcon icon={faTaxi} />
+                    <span>Airport Taxi</span>
+                  </div>
+            </div>
+                  { type !== 'list' &&
+                  <> 
+                  <h1 className="headerTitle">
+                        Explore your next adventure. It's Genius!
+                  </h1>
+                  <p className="headerDesc">
+                  See where you can travel to right now and find the best deals across thousands of flights, hotels and car hire options
+                  </p>
+                  <button className="headerBtn">Sign in/Register</button>
+                  <div className="headerSearch">
+                    <div className="headerSearchItem">
+                      <FontAwesomeIcon icon={faBed} className='headerIcon'/>
+                      <input type="text" placeholder='Where are you going?' className='headerSearchInput' />
+                    </div>
+
+                    <div className="headerSearchItem">
+                      <FontAwesomeIcon icon={faCalendarDays} className='headerIcon'/>
+                      <span onClick={()=> setOpenDate(!openDate) } className='headerSearchText'>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+                      { openDate && <DateRange
+                        editableDateInputs={true}
+                        onChange={item => setDate([item.selection])}
+                        moveRangeOnFirstSelection={false}
+                        ranges={date}
+                        className='date'
+                      />}
+                    </div>
+
+                    <div className="headerSearchItem">
+                      <FontAwesomeIcon icon={faPerson} className='headerIcon'/>
+                      <span onClick={()=> setOpenOpsions(!openOpsions)} className='headerSearchText'>{`${options.adult} adult • ${options.children} children • ${options.room} room` }</span>
+                        {openOpsions && <div className="options">
+                          <div className="optionItem">
+                            <span className='optionText'>Adult</span>
+                            <div className="optionCounter">
+                              <button disabled={options.adult <= 1} className="optionCounterButton" onClick={()=> handleOption("adult", "d")}>-</button>
+                              <span className="optionCounterNumber">{options.adult}</span>
+                              <button className="optionCounterButton" onClick={()=> handleOption("adult", "i")}>+</button>
+                            </div>
+                          </div>
+                          <div className="optionItem">
+                            <span className='optionText'>Children</span>
+                            <div className="optionCounter">
+                              <button disabled={options.children <= 0} className="optionCounterButton" onClick={()=> handleOption("children", "d")}>-</button>
+                              <span className="optionCounterNumber">{options.children}</span>
+                              <button className="optionCounterButton" onClick={()=> handleOption("children", "i")}>+</button>
+                            </div>
+                          </div>
+                          <div className="optionItem">
+                            <span className='optionText'>Room</span>
+                            <div className="optionCounter">
+                              <button disabled={options.room <= 1} className="optionCounterButton" onClick={()=> handleOption("room", "d")}>-</button>
+                              <span className="optionCounterNumber">{options.room}</span>
+                              <button className="optionCounterButton" onClick={()=> handleOption("room", "i")}>+</button>
+                            </div>
+                          </div>
+                        </div>}
+                    </div>
+
+                    <div className="headerSearchItem">
+                      <button className='headerBtn'>Search</button>
+                    </div>
+                  </div> </>}
+          </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './propertyList.css'
+
+
+const PropertyList = () => {
+  return (
+    <div className='pList'>
+          <div className="pListItem">
+                <img src="https://cache.marriott.com/marriottassets/Loyalty/program-pages/Use-Hotel-PointSavers-tile.jpg?interpolation=progressive-bilinear&downsize=600px:*" alt="" className="pListImg" />
+                  <div className="pListTitles">
+                        <h1>Hotes</h1>
+                        <h2>23232</h2>
+                  </div>
+          </div>
+
+          <div className="pListItem">
+                <img src="https://cache.marriott.com/marriottassets/Loyalty/program-pages/Use-Hotel-PointSavers-tile.jpg?interpolation=progressive-bilinear&downsize=600px:*" alt="" className="pListImg" />
+                  <div className="pListTitles">
+                        <h1>Hotes</h1>
+                        <h2>23232</h2>
+                  </div>
+          </div>
+
+          <div className="pListItem">
+                <img src="https://cache.marriott.com/marriottassets/Loyalty/program-pages/Use-Hotel-PointSavers-tile.jpg?interpolation=progressive-bilinear&downsize=600px:*" alt="" className="pListImg" />
+                  <div className="pListTitles">
+                        <h1>Hotes</h1>
+                        <h2>23232</h2>
+                  </div>
+          </div>
+
+          <div className="pListItem">
+                <img src="https://cache.marriott.com/marriottassets/Loyalty/program-pages/Use-Hotel-PointSavers-tile.jpg?interpolation=progressive-bilinear&downsize=600px:*" alt="" className="pListImg" />
+                  <div className="pListTitles">
+                        <h1>Hotes</h1>
+                        <h2>23232</h2>
+                  </div>
+          </div>
+
+          
+    </div>
+  )
+}
+
+export default PropertyList
